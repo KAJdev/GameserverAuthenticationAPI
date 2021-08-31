93644ea6fd09efccc8c52eb2b3b49ca301db7cf2
KAJ7 <zekewotring@gmail.com>
2021-08-30 21:22:39 -0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # GameserverAuthenticationAPI
  An API for game servers to authenticate player accounts (minecraft style)
+
+
+## Running the API
+
+I suggest you take this script as a base, and use your own database and data structs. This part explains how to start the API running. I also suggest that you use something like NGINX to reverse proxy so you can add complex caching layers and can scale vertically a bit better.
+
+### 1. Installing Libraries
+`pip3 install -r requirements.txt`
+
+### 2. Start the Server
+`python3 server.py`
+
+That should start the server. Now on to using it in your game.
+
+
+
+## Authenticating Players
+
+The authentication flow is quite simple. When a game client wants to connect to a server, it should send a request the `/authorize` with the username/password/deviceID or whatever else you want to use to authenticate players. The client will get get a one time use token that it should somehow communicate to the server it wants to connect to. Once the server has this token, the server can make its own independent request to `/grant` with the token to make sure that the player is who they say they are. The server should recieve a 200 OK with the player's data (retreived from database or however you get it). At that point, and only that point, can the player be allowed to spawn into the game.
+
+                     Client (1) --[user/pass]----\ 
+                     |   ^                        |
+                    (2)  |                        V
+                     |   \---------[token]------ API
+                  [token]                          ^
+                     |                             |
+                     V                             |
+                     GameServer (2) --[token]-----/
+
+I'm sorry, this diagram sucks... Hopefully you understand.
---
+++ server.py
@@ -0,0 +1,71 @@
+import asyncio
+from datetime import datetime
+from sanic import Sanic
+import sanic
+from sanic.log import logger as log
+from sanic.response import HTTPResponse, json
+import uuid
+import time
+
+app = Sanic("Public Game Auth API")
+app.config.FALLBACK_ERROR_FORMAT = "json"
+app.config.DEBUG = True
+
+token_cache = {}
+TIMEOUT_SECONDS = 120
+
+@app.post("/register")
+async def register(request: sanic.Request) -> HTTPResponse:
+    required = ['username', 'password', 'email', 'deviceID']
+    if not all(item in request.json.keys() for item in required):
+        return json({'code': 400, 'success': False, 'message': "Invalid request"}, 400)
+
+    # TODO: save information in database
+
+# Client sends user/pass and gets token to send to game server (Only valid for a few minutes, one time use)
+@app.post("/authorize")
+async def authenticate(request: sanic.Request) -> HTTPResponse:
+    required = ['username', 'password']
+    if not all(item in request.json.keys() for item in required):
+        return json({'code': 400, 'success': False, 'message': "Invalid request"}, 400)
+
+    # TODO: search authentication through database
+
+    token = str(uuid.uuid4())
+    token_cache[token] = {
+        'username': request.json.get('username'),
+        'nonce': time.time()
+    }
+
+    return json({
+        'code': 200,
+        'success': True,
+        'data': {
+            'token': token
+        }
+    }, 200)
+
+# game server sends token to get user information
+@app.post("/grant")
+async def grant(request: sanic.Request) -> HTTPResponse:
+    required = ['token']
+    if not all(item in request.json.keys() for item in required):
+        return json({'code': 400, 'success': False, 'message': "Invalid request"}, 400)
+
+    data = token_cache.get(request.json.get('token'), None)
+    if data is None:
+        return json({'code': 404, 'success': False, 'message': "User is not authenticated"}, 404)
+
+    if time.time() - data.get('nonce') > TIMEOUT_SECONDS:
+        del token_cache[request.json.get('token')]
+        return json({'code': 404, 'success': False, 'message': "User is not authenticated"}, 404)
+
+    return json({
+        'code': 200,
+        'success': True,
+        'data': data
+    }, 200)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 80, debug=False, access_log=False)
